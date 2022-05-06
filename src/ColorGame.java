dfb4011906730e806ea4ecda5f3200f0434ccc40
dylankrish <dylankrish@gmail.com>
2022-05-06 11:34:32 -0400

    Update ColorGame.java

--- src/ColorGame.java
+++ src/ColorGame.java
@@ -2,6 +2,8 @@ public class ColorGame {
     public static void main(String[] args) {
         // Create a new game
         ColorGame game = new ColorGame();
+        
+        
         System.out.println("Testing ColorGame");
 
 
