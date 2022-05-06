5e21777207acf3cba4b2c03380e1d4dcd2c2e9a8
dylankrish <dylankrish@gmail.com>
2022-05-06 09:24:51 -0400

    Testing

--- src/ColorGame.java
+++ src/ColorGame.java
@@ -1,11 +1,11 @@
 public class ColorGame {
     public static void main(String[] args) {
         // Create a new game
-        ColorGame game = new ColorGame();
+        // ColorGame game = new ColorGame();
+        System.out.println("Testing ColorGame");
 
 
     }
-
 
 
 
