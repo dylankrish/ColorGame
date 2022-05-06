4b635a7b10a9ba2bdf6e1fe44da3ba33a67a8c17
dylankrish <dylankrish@gmail.com>
2022-05-06 12:01:10 -0400

    Start on GUI

---
+++ src/ColorGameGUI.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+
+public class ColorGameGUI {
+    private JFrame frame;
+    private JPanel panel;
+
+    public ColorGameGUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
