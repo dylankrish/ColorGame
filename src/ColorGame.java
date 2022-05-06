2bf9756de6171c62d618764fdf979d79bd6812a9
dylankrish <dylankrish@gmail.com>
2022-05-06 10:14:33 -0400

    Export to Eclipse for Eclipse IDE Support

--- src/ColorGame.java
+++ src/ColorGame.java
@@ -1,12 +1,9 @@
 public class ColorGame {
     public static void main(String[] args) {
         // Create a new game
-        // ColorGame game = new ColorGame();
+        ColorGame game = new ColorGame();
         System.out.println("Testing ColorGame");
 
 
     }
-
-
-
 }
