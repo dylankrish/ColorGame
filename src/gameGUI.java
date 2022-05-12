c10a6877c9bb4ed34d0572a9d95a48aa6801d400
dylankrish <dylankrish@gmail.com>
2022-05-12 12:30:02 -0400

    do stuff

--- src/gameGUI.java
+++ src/gameGUI.java
@@ -1,17 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class gameGUI {
-    private JFrame frame;
-    private JPanel panel;
+public class gameGUI implements ActionListener {
 
     public gameGUI() {
-        frame = new JFrame();
-        panel = new JPanel();
+        int X = 5;
+        int Y = 5;
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel buttonPanel = new JPanel();//a new panel to hold all of the //buttons on the game board.
+        JButton[][] lightButtons;
+        lightButtons = new JButton[X][Y]; //creates a new two dimensional //ray that will hold 25 buttons.
+        buttonPanel.setLayout(new GridLayout(X,Y));
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new java.awt.GridLayout(X, Y));
 
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < Y; j++) {
+                JButton btn = new JButton();
+                lightButtons[i][j] = btn;
+                btn.setBorder(BorderFactory.createLineBorder(Color.blue)); // sets border color
+                btn.setBackground(Color.pink); // sets background color
+                contentPane.add(btn);
+            }
+        }
+        frame.add(contentPane);
         frame.setSize(500, 500);
         frame.setVisible(true);
         frame.add(panel);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
