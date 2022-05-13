e803fe2f0ac68f9d519fe68e7d7b055e0fba1f62
dylankrish <dylankrish@gmail.com>
2022-05-13 07:57:11 -0400

    Add border

--- src/gameGUI.java
+++ src/gameGUI.java
@@ -22,6 +22,7 @@ public class gameGUI implements ActionListener {
             for (int j = 0; j < Y; j++) {
                 JButton btn = new JButton();
                 lightButtons[i][j] = btn;
+                btn.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
                 btn.setBackground(new Color(0, 255, 168));
                 btn.setOpaque(true);
                 btn.setBorderPainted(false);
