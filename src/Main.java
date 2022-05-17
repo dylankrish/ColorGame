94d85ddf8b29bc6eb0a9959d473fbf2a52f93892
dylankrish <dylankrish@gmail.com>
2022-05-17 09:00:06 -0400

    Add RGB methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main extends gameGUI {
     public static void main(String[] args) {
 
-
         Main game = new Main();
 
     }
--- src/gameGUI.java
+++ src/gameGUI.java
@@ -23,6 +23,9 @@ public class gameGUI implements ActionListener {
                 JButton btn = new JButton();
                 lightButtons[i][j] = btn;
                 btn.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
+                int randomRed = randomRGB();
+                int randomGreen = randomRGB();
+                int randomBlue = randomRGB();
                 btn.setBackground(new Color(0, 255, 168));
                 btn.setOpaque(true);
                 btn.setBorderPainted(false);
@@ -35,9 +38,14 @@ public class gameGUI implements ActionListener {
         frame.add(panel);
 
     }
-
+    public int randomRGB() {
+        return (int)(Math.random() * 255);
+    }
+    public int variationRGB(int color) {
+        return (int)(Math.random() * color);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println(e);
     }
 }
